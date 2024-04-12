ae71dbaa6d11d1245acf97e52cc27f1283cd5aa3
Glenn444 <gmark1586@gmail.com>
2024-04-12 16:29:22 +0300

    Upload Button

--- app/[schoolId]/products/[id]/upload/page.tsx
+++ app/[schoolId]/products/[id]/upload/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ProductImage } from "@/app/actions";
+import { Button } from "@/components/ui/button";
 import { CldImage, CldUploadButton } from "next-cloudinary";
 
 
@@ -19,11 +20,13 @@ export default function Home({ params }: { params: { id: string } }) {
       await ProductImage(params.id, event);
   };
   return (
-    <main className="flex flex-col my-6">
+    <main className="flex flex-col my-6 justify-center items-center ">
       <h2 className="pb-2 text-xl font-semibold">Upload Product Image below</h2>
+      <Button>
       <CldUploadButton uploadPreset="en5vjmdx"
       onSuccess={handleUploadSuccess as any}
        />
+       </Button>
     </main>
   );
 }
